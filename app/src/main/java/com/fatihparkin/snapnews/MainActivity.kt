046d569947b09a989d9d54b76fad1abc0d76f4ed
FatihParkın <61151029+fatihparkin@users.noreply.github.com>
2025-02-28 17:37:31 +0300

    New Project

--- app/src/main/java/com/fatihparkin/snapnews/MainActivity.kt
+++ app/src/main/java/com/fatihparkin/snapnews/MainActivity.kt
@@ -11,5 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        println("deneme")
+
     }
 }

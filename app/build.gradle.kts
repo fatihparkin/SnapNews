96716809d86c03eaf99fa41d6af46e379e42fabc
FatihParkın <61151029+fatihparkin@users.noreply.github.com>
2025-03-03 18:21:19 +0300

    Veri çekme ve listeleme işlemleri yapıldı

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,9 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
@@ -45,4 +48,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 }
--- app/src/main/java/com/fatihparkin/snapnews/MainActivity.kt
+++ app/src/main/java/com/fatihparkin/snapnews/MainActivity.kt
@@ -1,17 +1,75 @@
 package com.fatihparkin.snapnews
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.fatihparkin.snapnews.databinding.ActivityMainBinding
+import com.google.gson.annotations.SerializedName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+//Data classes
+
+data class NewsResponse(
+    @SerializedName("articles") val articles: List<Article>
+)
+
+data class Article(
+    @SerializedName("source") val source: Source,
+    @SerializedName("title") val title: String,
+    @SerializedName("urlToImage") val imageUrl: String?,
+    @SerializedName("publishedAt") val publishedAt: String,
+)
+
+data class Source(
+    @SerializedName("name") val name: String
+)
+
+//APİ
+object RetrofitClient {
+    private const val BASE_URL = "https://newsapi.org/v2/"
+
+    val api: NewsApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(NewsApi::class.java)
+    }
+}
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        println("deneme")
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+
+        fetchNews()
+    }
+
+    private fun fetchNews() {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val response = RetrofitClient.api.getNews()
+                if (response.isSuccessful) {
+                    val newsList = response.body()?.articles ?: emptyList()
+                    runOnUiThread {
+                        //Recycler view
+                        binding.recyclerView.adapter = NewsAdapter(newsList)
+                    }
+                }
+            } catch (e: Exception) {
+                Log.e("MainActivity", "Hata: ${e.message}")
+            }
+        }
     }
 }
